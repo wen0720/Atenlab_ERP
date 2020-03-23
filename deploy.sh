9b078027f382eb67f249d0e88afc20ad25270297
Nick <icguanyu@gmail.com>
2020-03-23 18:09:02 +0800

    新增部屬demo至gh-pages的腳本

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'auto deploy to gh-pages'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+git push -f https://github.com/AllPower-in/Atenlab_ERP.git master:gh-pages
+
+cd -
+
+
+# (執行腳本)sh ./deploy.sh
